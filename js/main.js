7949b38b077c8c2f1bca5a74af32e46c76a47614
Boyarko Maxim <martiniden1297@gmail.com>
2022-02-15 13:42:19 +0300

    unit:start progect

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+$('.header-slider').slick({
+    dots: true, 
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/Vector.svg" alt="vector"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/Vector1.svg" alt="vector1"></button>'
+});
+});
