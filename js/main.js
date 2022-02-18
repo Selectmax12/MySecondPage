3bd0bcd10c59cca0503df052e751024bde5ecd01
Boyarko Maxim <martiniden1297@gmail.com>
2022-02-18 19:29:47 +0300

    feat:body full

--- js/main.js
+++ js/main.js
@@ -4,4 +4,27 @@ $('.header-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><img src="images/Vector.svg" alt="vector"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/Vector1.svg" alt="vector1"></button>'
 });
+
+
+
+$('.product__name').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    vertical: true,
+    prevArrow: '<button type="button" class="product-prev"><img src="images/Vector22.svg" alt="vector"></button>',
+    nextArrow: '<button type="button" class="product-next"><img src="images/Vector44.svg" alt="vector1"></button>',
+    asNavFor: '.product__content'
+    
+  });
+  $('.product__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.product__name',
+    fade: true,
+    arrows: false
+  });
+
+
+
 });
